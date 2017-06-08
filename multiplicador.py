a0cdb2c938c5d2a67996a0b0bd6c258d8467738a
Walter Silvestre Coan <walter.coan@gmail.com>
2017-06-07 21:19:15 -0300

    Código da aula 30/05/2017

---
+++ multiplicador.py
@@ -0,0 +1,15 @@
+num1 = 0
+num2 = 0
+
+print("Digite o primeiro numero")
+num1 = int(input())
+print("Digite o segundo numero")
+num2 = int(input())
+cont=0
+soma=0
+while(cont < num1):
+    cont = cont + 1
+    print(num2,end=" + ")
+     soma= soma + num2
+
+print("O resultado e", soma)
--- repeticao.py
+++ repeticao.py
@@ -2,11 +2,40 @@
 while(True):
     print("Mae eu fiz a matrix...")
     time.sleep(0.1)
-'''
+
 v=0
 while(v <= 10):
     print(v)
     v = v + 1
+
+import time
+v=10
+while(v >= 0):
+    print(v)
+    v = v - 1
+    time.sleep(1)
+'''
+
+i = 0
+soma = 0
+while (i < 200):
+    i = i + 1
+    if i % 2 == 1:
+        print(i,end=" ")
+        soma = soma + i
+        if i % 11 ==0:
+            print()
+print("\nA soma dos 100 primeiros impares e", soma)
+
+
+
+
+
+
+
+
+
+
 
 
 
